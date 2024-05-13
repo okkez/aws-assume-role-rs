8ca365fe45a1b6799e5d48fcad9847ee7f6f707f
okkez <okkez000@gmail.com>
2024-05-14 08:45:43 +0900

    refactor: set STS client as a parameter

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
 use aws_assume_role::cli::Cli;
 use clap::Parser;
+use aws_config::BehaviorVersion;
+use aws_sdk_sts as sts;
 
 
 #[::tokio::main]
 async fn main() {
     let cli = Cli::parse();
-    cli.execute().await.unwrap();
+
+    let config = aws_config::defaults(BehaviorVersion::latest())
+        .retry_config(aws_config::retry::RetryConfig::standard().with_max_attempts(3))
+        .load()
+        .await;
+    let sts = sts::Client::new(&config);
+
+    cli.execute(&sts).await.unwrap();
 }
