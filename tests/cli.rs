89bbed7c9ca2c2ac4094d183b15c5ff815d3cee9
okkez <okkez000@gmail.com>
2024-05-18 00:19:52 +0900

    feat: improve arguments validation

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,5 +1,6 @@
 use aws_assume_role::cli::Cli;
 use clap::Parser;
+use rstest::rstest;
 
 #[test]
 fn verify_cli() {
@@ -13,8 +14,11 @@ fn no_arguments() {
     assert!(!cli.is_ok());
 }
 
-#[test]
-fn serial_number() {
-    let cli = Cli::try_parse_from(["assume-role", "--serial-number=test_serial_number"]);
-    assert!(cli.is_ok());
+#[rstest]
+#[case("", false)]
+#[case("--totp-code=123456", true)]
+#[case("--totp-secret=secret", true)]
+fn serial_number(#[case] arg: &str, #[case] success: bool) {
+    let cli = Cli::try_parse_from(["assume-role", "--serial-number=test_serial_number", arg]);
+    assert_eq!(cli.is_ok(), success);
 }
